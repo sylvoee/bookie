1a77555c8e0c7ec3b52069bd04d1cbf03382be9c
Ehiemuan Sylvester Ose <sylvoee@gmail.com>
2022-11-19 17:43:00 +0100

    Update header.php

--- header.php
+++ header.php
@@ -1,9 +1,9 @@
 
-<!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet">
    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
 
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <link href="style.css" rel="stylesheet"> -->
+  <link href="style.css" rel="stylesheet">
   
    <!-- Offline bootstrap and font-awesome -->
    
